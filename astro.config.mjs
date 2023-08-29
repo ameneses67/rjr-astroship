b7fd5c59e4a5824a8ee6ce7d9270b8c4c2d928da
Poncho <ameneses67@gmail.com>
2023-08-29 15:23:28 -0600

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:3000/",
+  site: "https://retirate-joven-y-rico.pages.dev",
   integrations: [mdx(), sitemap(), tailwind()],
   experimental: {
     assets: true,
